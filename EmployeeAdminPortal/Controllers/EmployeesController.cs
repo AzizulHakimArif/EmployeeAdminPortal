42038c5b8254f29bf77e17ae268d0267110470bf
AzizulHakimArif <User@DESKTOP-0UBQBM5>
2024-08-21 03:31:15 +0600

    Add project files.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbcontext dbcontext;
+
+        public EmployeesController(ApplicationDbcontext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+
+        [HttpGet]
+
+        public IActionResult GetAllEmployees()
+        {
+           var allEmployees = dbcontext.Employees.ToList();
+
+            return Ok(allEmployees);
+
+
+        }
+
+        [HttpPost]
+
+        public IActionResult AddEmployee()
+        {
+
+        }
+
+
+    }
+}
